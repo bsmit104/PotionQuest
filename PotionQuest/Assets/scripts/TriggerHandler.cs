0484cbd55608823de4f9c3d574eabce2701af607
Brayden Smith <bsmit104@ucsc.edu>
2024-07-26 00:02:19 -0700

    fixed prison bar trigger

--- PotionQuest/Assets/scripts/TriggerHandler.cs
+++ PotionQuest/Assets/scripts/TriggerHandler.cs
@@ -7,12 +7,12 @@ public class TriggerHandler : MonoBehaviour
     public GameObject closed;
     public GameObject open;
     public string itemName = "Strength Potion";
+    public Inventory inventory; // Public Inventory field
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Inventory inventory = other.GetComponent<Inventory>();
             if (inventory != null && inventory.GetItemByName(itemName) != null)
             {
                 closed.SetActive(false);
